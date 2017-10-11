718af1f5f06f43b2d2bdce08a0731f4669c32187
root <root@DESKTOP-RLPVRQV.localdomain>
2017-10-11 22:47:22 +0200

    push bloqué

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -28,5 +28,6 @@ export class UserComponent {
       var evt = document.createEvent("MouseEvents");
       evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
       document.querySelector('[aria-label="Play/Pause"]').dispatchEvent(evt);      
+      
   }
 }

261f47366301de48c44b4bc4b7a0b3dda1701b6f
root <root@DESKTOP-RLPVRQV.localdomain>
2017-10-11 21:41:19 +0200

    click bouton play

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -28,5 +28,9 @@ export class UserComponent {
       var evt = document.createEvent("MouseEvents");
       evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
       document.querySelector('[aria-label="Play/Pause"]').dispatchEvent(evt);
+<<<<<<< 8d9f5e098a977dbbbba3774576ec630ee64de85a
+=======
+      
+>>>>>>> click bouton play
   }
 }
